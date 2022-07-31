45d3152bdf0f418ab795de57126a3fa26e2cc387
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-07-30 22:12:22 -0500

    Create About Me Page

--- src/comps/Banner.js
+++ src/comps/Banner.js
@@ -13,7 +13,6 @@ export default function Banner() {
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>{`Hi! I'm Joy Khweis`}</h1>
             <h2>{`Junior React Developer`}</h2>
-            <p>Loren ipsum </p>
             <a
               href="mailto:joykhweis@gmail.com"
               target="_blank"
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,53 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+//styles & icons
 import './About.css';
 
 export default function About() {
   return (
     <section className="about" id="about">
-      About
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={5} xl={7}>
+            <h2>{`About Me`}</h2>
+            <span className="tagline">
+              <p>
+                Hello! I am a self-taught Junior React Developer with a passion
+                for creativity and clean code. I discovered my love of coding
+                during the pandemic when I signed up for a SheCodes course. From
+                there, I completed all available SheCodes courses in JavaScript,
+                React, and Responsive. I also completed Shaun Pelling's (The Net
+                Ninja) Udemy Course on React and Firebase which helped me to
+                further hone my skills. I have since been building my own
+                projects in React. I am currently looking to gain more
+                experience under the wing of senior developers and am seeking
+                apprenticeship or work opportunities.
+              </p>
+            </span>
+          </Col>
+          <Col xs={12} md={5} xl={7}>
+            <h2>{`Skills`}</h2>
+            <span className="tagline">
+              <p>
+                <ul>
+                  <li>React Functional Components</li>
+                  <li>React Hooks (useState, useEffect, useRef, useReducer)</li>
+                  <li>Custom Hooks</li>
+                  <li>Context</li>
+                  <li>React Router Dom Version 6</li>
+                  <li>Firebase authentication, realtime database & hosting</li>
+                  <li>API's</li>
+                  <li>Bootstrap</li>
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                </ul>
+                <br />
+                <br />
+              </p>
+            </span>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
